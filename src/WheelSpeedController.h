f957e83944c6c235871bfcb5dfa99b374eae55cd
Andreas Coroiu <andreas.coroiu@gmail.com>
2021-05-15 11:09:13 +0200

    Working and calibrated wheel speed controller.

--- src/WheelSpeedController.h
+++ src/WheelSpeedController.h
@@ -2,11 +2,12 @@
 #define _WHEEL_SPEED_CONTROLLER_H
 
 #define MAX_INPUT 1000
+// #define WHEEL_SPEED_CONTROLLER_P 0.002
 #define WHEEL_SPEED_CONTROLLER_P 0.002
-// #define WHEEL_SPEED_CONTROLLER_P 0
-#define WHEEL_SPEED_CONTROLLER_I 0
-#define WHEEL_SPEED_CONTROLLER_D 0.0001
-#define WHEEL_SPEED_CONTROLLER_F 1.0 / (9.0 * 100.0)
+#define WHEEL_SPEED_CONTROLLER_I 0.0005
+#define WHEEL_SPEED_CONTROLLER_D 0
+#define WHEEL_SPEED_CONTROLLER_F 0
+// #define WHEEL_SPEED_CONTROLLER_F 1.0 / (9.0 * 100.0)
 
 #include <MiniPID.h>
 #include <DataLogger.h>
@@ -29,7 +30,10 @@ public:
         wheel(wheel),
         motor(motor)
   {
-    controller.setOutputLimits(-100.0, 100.0);
+    controller.setOutputLimits(-1.0, 1.0);
+    controller.setOutputFilter(.1);
+    controller.setOutputRampRate(.2);
+    controller.setSetpointRange(10);
     dataLogger->addVariable(500, VariableLevel::Public, output);
   }
 
